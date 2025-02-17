a022abb9f5dcdc0f3600caaeb1ade8cd0dd0baa8
OrangeMintz <nickzgacus@gmail.com>
2025-02-17 16:07:48 +0800

    Added confusing code

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,17 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\SalesController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:api');
+
+Route::post("register", [AuthController::class,"register"]);
+Route::post("login", [AuthController::class,"login"]);
+
+Route::middleware("auth:api",)->group(function(){
+    Route::apiResource("sales", SalesController::class);
+});
