879fe5fb60862a3b60cc57b8e2e41bff529d679d
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-02-18 14:00:03 +0800

    add transaction-list page segment header

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 Route::get('/register', function () {
     return view('register');
-});
+})->name('register');
 
 Route::get('/transaction', function () {
     return view('transaction');
-});
+})->name('transaction');
+
+Route::get('/transaction-list', function () {
+    return view('transaction-list');
+})->name('transaction-list');
