c9b3ec31fae9d345ec4df6ac39f33f9a38d7564f
OrangeMintz <nickzgacus@gmail.com>
2025-02-18 22:47:47 +0800

    Update: -Migrated Views and minor adjustments

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransactionsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,9 +13,16 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+
+    // PROFILE MANAGEMENT
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // TRANSACTIONS
+    Route::get('/transactions', [TransactionsController::class, 'index'])->name('transactions');
+    Route::get('/transactions-list', [TransactionsController::class, 'list'])->name('transactions.list');
+
 });
 
 require __DIR__.'/auth.php';
