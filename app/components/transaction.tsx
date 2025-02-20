48a1a5fef98e8008b7be12eaf0889ea11c2a9e21
Jeffrey Sedoro <jeffreysedoro5@gmail.com>
2025-02-20 11:34:24 +0800

    putting name on it, cause why not? HAHHAHAHA

--- app/components/transaction.tsx
+++ app/components/transaction.tsx
@@ -11,72 +11,72 @@ export default function CashierForm() {
                         <div className="bg-white mb-4 shadow-md grid md:grid-cols-3 sm:grid-cols-2 gap-4 p-4">
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Cash:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="cash" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Check:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number"  name="check" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">BPI Credit Card:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="bpi_ccard" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">BPI Debit Card:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="bpi_dcard" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Metro Credit Card:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="metro_ccard" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Metro Debit Card:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="metro_dcard" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Pay Maya:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="paymaya" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">AUB Credit Card:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="aub_ccard" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">GCash:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="gcash" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Food Panda:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="foodpanda" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">StreetBy:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="streetby" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Grab Food:</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="grabfood" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">GC Claimed (Others):</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="gc_claimed_others" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
 
                             <div className="w-full">
                                 <label className="block text-sm font-medium">GC Claimed (OWN):</label>
-                                <input type="number" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
+                                <input type="number" name="gc_claimed_own" className="w-full p-2 border border-gray-300 rounded-md" step="0.01" />
                             </div>
                         </div>
 
@@ -84,27 +84,27 @@ export default function CashierForm() {
                             <div className="bg-white mb-4 shadow-md p-4 grid md:grid-cols-2 gap-4">
                                 <div className="w-full">
                                     <label className="block text-sm font-medium">MM-HEAD OFFICE:</label>
-                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md" />
+                                    <input type="text" className="w-full p-3 border  border-gray-300 rounded-md" name="mm_head_office" />
                                 </div>
 
                                 <div className="w-full">
                                     <label className="block text-sm font-medium">MM-COMMISSARY:</label>
-                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md" />
+                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md"  name="mm_commissary" />
                                 </div>
 
                                 <div className="w-full">
                                     <label className="block text-sm font-medium">MM-RM:</label>
-                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md" />
+                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md"  name="mm_rm" />
                                 </div>
 
                                 <div className="w-full">
                                     <label className="block text-sm font-medium">MM-DM:</label>
-                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md" />
+                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md" name="mm_dm" />
                                 </div>
 
                                 <div className="w-full">
                                     <label className="block text-sm font-medium">FOOD CHARGE:</label>
-                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md" />
+                                    <input type="text" className="w-full p-3 border border-gray-300 rounded-md"  name="food_charge"/>
                                 </div>
                             </div>
 
@@ -112,7 +112,7 @@ export default function CashierForm() {
                         <h2 className="shadow-md font-semibold text-lg mb-4 p-4">Z Reading POS:</h2>
                         <div className="bg-white mb-4 shadow-md p-4">
                             <label className="block text-sm font-medium">Z READING POS:</label>
-                            <input type="text" className="w-full p-3 border border-gray-300 rounded-md" />
+                            <input type="text" className="w-full p-3 border border-gray-300 rounded-md" name="z_reading_pos" />
                         </div>
 
                         <div className="flex items-center justify-start m-4">
@@ -129,7 +129,7 @@ export default function CashierForm() {
                         <div className="grid grid-cols-1 gap-4 p-4 mb-8">
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Shift Time:</label>
-                                <select className="w-full p-2 border border-gray-300 rounded-md">
+                                <select className="w-full p-2 border  border-gray-300 rounded-md" name="shift_time">
                                     <option value="AM">AM</option>
                                     <option value="MID">MID</option>
                                     <option value="PM">PM</option>
@@ -137,7 +137,7 @@ export default function CashierForm() {
                             </div>
                             <div className="w-full">
                                 <label className="block text-sm font-medium">Cashier&apos;s Name:</label>
-                                <input type="text" className="w-full p-2 border border-gray-300 rounded-md" required />
+                                <input type="text" className="w-full p-2 border border-gray-300 rounded-md" name="cashier_name" required />
                             </div>
                         </div>
 
