03262c91d832663f6e9292f98bdf056442ce3fb8
Amiel2003 <2101102216@student.buksu.edu.ph>
2025-02-20 09:37:04 +0800

    logout frontend

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -37,6 +37,34 @@ export default function Dashboard() {
         }
       };
 
+    const logout = async (event: React.FormEvent) => {
+      event.preventDefault();
+
+      try {
+        const token = localStorage.getItem("access_token");
+
+        const response = await axios.post("http://127.0.0.1:8000/api/logout",{},{
+          headers: {
+            Authorization: `Bearer ${token}`, 
+            Accept: "application/json",
+          },
+        })
+
+        console.log(response.data)
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+
+        router.push('/')
+  
+        if (!token) {
+          console.error("No access token found.");
+          return;
+        }
+      } catch (error) {
+        console.error("Error logging out: ", error)
+      }
+    }
+
     useEffect(()=>{
         getUser();
     },[])
@@ -54,6 +82,10 @@ export default function Dashboard() {
                             <p>{user.name}</p>
                             <p>{user.email}</p>
                         </div>: ''}
+
+                <form onSubmit={logout}>
+                  <button type="submit" className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Log out</button>
+                </form>
                 
               </div>
             </div>
