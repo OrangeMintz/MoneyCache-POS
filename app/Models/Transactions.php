ee5db0b0575f3fffa689fc18369e148a620a6bbb
Amiel2003 <2101102216@student.buksu.edu.ph>
2025-02-21 08:58:21 +0800

    - store transaction api and foreign key cashier

--- app/Models/Transactions.php
+++ app/Models/Transactions.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Transactions extends Model
 {
@@ -16,5 +17,13 @@ class Transactions extends Model
         'grabfood','gc_claimed_others','gc_claimed_own','mm_head','mm_commissary','mm_rm',
         'mm_dm','mm_km','food_charge','z_reading_pos','sub_total_trade',
         'sub_total_non_trade','grand_total', 'over_pos'];
+
+    
+    public function cashier()
+    {
+        return $this->belongsTo(User::class, 'cashier');
+    }
+
+    
 
 }
