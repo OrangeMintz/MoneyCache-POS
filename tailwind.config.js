6ec5e8b72c07e45f3beb607adbea22ba5d158d7e
OrangeMintz <nickzgacus@gmail.com>
2025-03-03 13:56:32 +0800

    Add Welcome Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,13 +17,16 @@ export default {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
             colors: {
+                TWHITE: "#FAFAFA",
                 MCGreen: "#6ABD45",
+                MCGreenHover: "#589f3a",
                 TableLYellow: "#FAF2C7",
-                TableBlue:"#1D2D5C",
-                TableYellow:"#F3D963",
-                TableOrange:"#AC881B",
+                TableBlue: "#1D2D5C",
+                TableYellow: "#F3D963",
+                TableOrange: "#AC881B",
                 TableDOrange: "#6E5811",
-            },
+                primary: { "50": "#eff6ff", "100": "#dbeafe", "200": "#bfdbfe", "300": "#93c5fd", "400": "#60a5fa", "500": "#3b82f6", "600": "#2563eb", "700": "#1d4ed8", "800": "#1e40af", "900": "#1e3a8a", "950": "#172554" }
+            }
         },
     },
 
