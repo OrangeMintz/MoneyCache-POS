34dd21465686e00c88f2f79ee7a73ac04273f0a4
Amiel2003 <2101102216@student.buksu.edu.ph>
2025-03-03 11:00:10 +0800

    - delet user api route

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::middleware(['auth:api'])->group(function () {
             Route::get('/', [UserController::class, 'index']);
             Route::post('/', [UserController::class, 'store']);
             Route::put('/{id}', [UserController::class, 'update']);
+            Route::delete('/{id}', [UserController::class, 'softDelete']);
         });
 
     });
