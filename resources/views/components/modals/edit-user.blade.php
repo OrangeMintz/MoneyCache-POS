83c313d78a5b9344811be50b4ff220a3b8af273f
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-03-11 21:41:19 +0800

    fix: edit & add user modal data-close-modal

--- resources/views/components/modals/edit-user.blade.php
+++ resources/views/components/modals/edit-user.blade.php
@@ -1,72 +1,72 @@
 <!-- Main Modal -->
 @foreach ($users as $user)
     <div id="updateModal{{ $user->id }}" tabindex="-1" aria-hidden="true"
-        class="hidden fixed inset-0 z-50 flex items-center justify-center">
-        <!-- Overlay -->
-        <div class="absolute inset-0 bg-gray-900 bg-opacity-50"></div>
+        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full bg-black bg-opacity-50">
+        <div class="relative p-4 w-full max-w-md max-h-full">
 
-        <!-- Modal Content -->
-        <div class="relative p-4 w-full max-w-md bg-white rounded-lg shadow dark:bg-gray-700 z-50">
-            <!-- Modal Header -->
-            <div class="flex items-center justify-between border-b border-gray-200 dark:border-gray-600 rounded-t">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Update User
-                </h3>
-                <button type="button"
-                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-close-modal>
-                    <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 14 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-            </div>
-
-            <!-- Modal Body -->
-            <form class="p-1 md:p-2" method="POST" action="{{ route('admin.update', $user->id) }}">
-                @csrf
-                @method('PUT')
-                <div class=" mb-4 md:mb-8 grid-cols-2">
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="role"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
-                        <select name="role" id="role" required
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white">
-                            <option value="admin" {{ $user->role === 'admin' ? 'selected' : '' }}>Admin</option>
-                            <option value="cashier" {{ $user->role === 'cashier' ? 'selected' : '' }}>Cashier</option>
-                        </select>
-                    </div>
-                    <div class="col-span-2">
-                        <label for="name"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                        <input type="text" name="name" id="name"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                            placeholder="Enter New Name" value="{{ $user->name }}" required>
-                    </div>
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="email"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                        <input type="email" name="email" id="email"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                            placeholder="Enter New Email" value="{{ $user->email }}" required>
-                    </div>
-
-                    <div class="col-span-2 sm:col-span-1">
-                        <label for="password"
-                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                        <input type="password" name="password" id="password"
-                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
-                            placeholder="Enter New Password">
-                    </div>
-
+            <!-- Modal Content -->
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <!-- Modal Header -->
+                <div class="flex items-center justify-between border-b border-gray-200 dark:border-gray-600 rounded-t p-4">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                        Update User
+                    </h3>
+                    <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="updateModal{{ $user->id }}">
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
                 </div>
-                <button type="submit"
-                    class="w-full py-2.5 px-5 text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center justify-center">
-                    Update user
-                </button>
-            </form>
+
+                <!-- Modal Body -->
+                <form class="p-4 md:p-2" method="POST" action="{{ route('admin.update', $user->id) }}">
+                    @csrf
+                    @method('PUT')
+                    <div class="p-4 mb-4 md:mb-8 grid-cols-2">
+                        <div class="col-span-2 sm:col-span-1">
+                            <label for="role"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Role</label>
+                            <select name="role" id="role" required
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white">
+                                <option value="admin" {{ $user->role === 'admin' ? 'selected' : '' }}>Admin</option>
+                                <option value="cashier" {{ $user->role === 'cashier' ? 'selected' : '' }}>Cashier</option>
+                            </select>
+                        </div>
+                        <div class="col-span-2">
+                            <label for="name"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+                            <input type="text" name="name" id="name"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
+                                placeholder="Enter New Name" value="{{ $user->name }}" required>
+                        </div>
+                        <div class="col-span-2 sm:col-span-1">
+                            <label for="email"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                            <input type="email" name="email" id="email"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
+                                placeholder="Enter New Email" value="{{ $user->email }}" required>
+                        </div>
+
+                        <div class="col-span-2 sm:col-span-1">
+                            <label for="password"
+                                class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" name="password" id="password"
+                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:text-white"
+                                placeholder="Enter New Password">
+                        </div>
+
+                    </div>
+                    <button type="submit"
+                        class="w-full py-2.5 px-5 text-sm font-medium text-white bg-blue-500 hover:bg-blue-600 rounded-lg focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 flex items-center justify-center">
+                        Update user
+                    </button>
+                </form>
+            </div>
         </div>
     </div>
 @endforeach
--- resources/views/pages/attendance.blade.php
+++ resources/views/pages/attendance.blade.php
@@ -12,6 +12,7 @@
                       <th>Name</th>
                       <th>Time in</th>
                       <th>Time out</th>
+                      <th>Attendance Status</th>
                       <th>Number of Hours</th>
                       <th>Date</th>
                   </tr>
@@ -21,43 +22,49 @@
                       <td>Tiger Nixon</td>
                       <td>System Architect</td>
                       <td>Edinburgh</td>
+                      <td>Early</td>
                       <td>61</td>
-                      <td>61</td>
+                      <td>6122-22-22</td>
                   </tr>
                   <tr>
                       <td>Garrett Winters</td>
                       <td>Accountant</td>
                       <td>Tokyo</td>
+                      <td>Late</td>
                       <td>63</td>
-                      <td>63</td>
+                      <td>6333-33-33</td>
                   </tr>
                   <tr>
                       <td>Ashton Cox</td>
                       <td>Junior Technical Author</td>
                       <td>San Francisco</td>
+                      <td>Overtime</td>
                       <td>66</td>
-                      <td>66</td>
+                      <td>6622-22-22</td>
                   </tr>
                   <tr>
                       <td>Cedric Kelly</td>
                       <td>Senior Javascript Developer</td>
                       <td>Edinburgh</td>
+                      <td>Late</td>
                       <td>66</td>
-                      <td>22</td>
+                      <td>2222-22-22</td>
                   </tr>
                   <tr>
                       <td>Airi Satou</td>
                       <td>Accountant</td>
                       <td>Tokyo</td>
+                      <td>Overtime</td>
                       <td>66</td>
-                      <td>33</td>
+                      <td>3312-23-22</td>
                   </tr>
                   <tr>
                       <td>Brielle Williamson</td>
                       <td>Integration Specialist</td>
                       <td>New York</td>
+                      <td>Overtime</td>
                       <td>66</td>
-                      <td>61</td>
+                      <td>6112-23-23</td>
                   </tr>
               </tbody>
           </table>
