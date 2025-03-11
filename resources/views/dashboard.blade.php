9a1061fdde021549ed30701b5db09571adf4f8cb
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-03-11 09:48:57 +0800

    fix: darkmode on dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
               </div>
             </div>
             <section class="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
-                <div class="flex items-center p-8 bg-white shadow rounded-lg">
+                <div class="flex items-center p-8 bg-white dark:bg-gray-800 shadow rounded-lg">
                     <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-purple-600 bg-purple-100 rounded-full mr-6">
                     <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
@@ -30,7 +30,7 @@
                     <span class="block text-gray-500">Users</span>
                     </div>
                 </div>
-                <div class="flex items-center p-8 bg-white shadow rounded-lg">
+                <div class="flex items-center p-8 bg-white dark:bg-gray-800 shadow rounded-lg">
                     <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-green-600 bg-green-100 rounded-full mr-6">
                     <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6" />
@@ -41,7 +41,7 @@
                     <span class="block text-gray-500">Transaction</span>
                     </div>
                 </div>
-                <div class="flex items-center p-8 bg-white shadow rounded-lg">
+                <div class="flex items-center p-8 bg-white dark:bg-gray-800 shadow rounded-lg">
                     <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-red-600 bg-red-100 rounded-full mr-6">
                     <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 17h8m0 0V9m0 8l-8-8-4 4-6-6" />
@@ -52,7 +52,7 @@
                     <span class="block text-gray-500">Net Sales</span>
                     </div>
                 </div>
-                <div class="flex items-center p-8 bg-white shadow rounded-lg">
+                <div class="flex items-center p-8 bg-white dark:bg-gray-800 shadow rounded-lg">
                     <div class="inline-flex flex-shrink-0 items-center justify-center h-16 w-16 text-blue-600 bg-blue-100 rounded-full mr-6">
                     <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
@@ -67,7 +67,7 @@
             <section class="grid md:grid-cols-3 xl:grid-cols-3 md:grid-rows-2 xl:grid-flow-col gap-6">
 
                 {{-- chart --}}
-                <div class="flex flex-col row-span-3 md:col-span-2 md:row-span-2 bg-white shadow rounded-lg p-4 sm:p-8">
+                <div class="flex flex-col row-span-3 md:col-span-2 md:row-span-2 bg-white shadow rounded-lg p-4 sm:p-8 dark:bg-gray-800 dark:text-white">
                     
                     <h5 class="text-xl font-bold leading-none text-gray-900 dark:text-white">Income</h5>
                     <div class="flex-grow">
@@ -138,7 +138,7 @@
 
 
                 {{-- recent logs --}}
-                <div class="row-span-2  md:col-span-1 bg-white shadow rounded-lg">
+                <div class="row-span-2  md:col-span-1 bg-white dark:bg-gray-800 shadow rounded-lg">
 
 
                     <div class="w-full max-w-md p-4 bg-white border border-gray-200 rounded-lg shadow-sm sm:p-8 dark:bg-gray-800 dark:border-gray-700">
