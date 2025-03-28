eb8a3523922a3a00f9a714a77b0e6ff999be94fe
ramzel1414 <ramonpaulocaumban@gmail.com>
2025-03-28 10:30:58 +0800

    my cahnges in profile

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,26 +5,48 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-full mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+    <div class="font-sans bg-gray-100 dark:bg-gray-800  dark:text-gray-100 p-6">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
+                <!-- First Column: Profile & Password Update -->
+                    <div class="p-4 sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
+                        <div class="max-w-full">
+                            @include('profile.partials.update-profile-information-form')
+                        </div>
+                    </div>
+            
+                    <div class="p-4 sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
+                        <div class="max-w-xl">
+                            @include('profile.partials.update-password-form')
+                        </div>
+                    </div>
+            
+                <!-- Second Column: Delete User (Full Width) -->
+                <div class="md:col-span-2">
+                    <div class="p-4 sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
+                        <div class="max-w-xl">
+                            @include('profile.partials.delete-user-form')
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            {{-- <div class="p-4 sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
                 <div class="max-w-full">
                     @include('profile.partials.update-profile-information-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-password-form')
                 </div>
             </div>
 
-            <div class="p-4 sm:p-8 bg-white dark:bg-gray-800 shadow sm:rounded-lg">
+            <div class="sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.delete-user-form')
                 </div>
-            </div>
-        </div>
+            </div> --}}
     </div>
 @include('layouts.footer')
 
--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -67,16 +67,16 @@
             </div>
 
             <!-- Right Side (Image & Upload) -->
-            <div class="right col-span-1 flex flex-col justify-center items-center">
+            <div class="right col-span-1 flex flex-col">
                 <!-- Profile Image -->
                 <label for="profile-image" class="cursor-pointer">
-                    <img id="previewImage" src="{{ asset('img/user.png') }}" alt="User Image" class="w-40 h-40 rounded-full border-2 border-gray-300">
+                    <img id="previewImage" src="{{ asset('img/user.png') }}" alt="User Image" class="w-36 h-36 mx-auto rounded-full border-2 border-gray-300">
                 </label>
                 
                 <!-- Upload Button -->
                 <input type="file" id="profile-image" name="profile_image" class="hidden" accept="image/*" onchange="previewFile()">
                 
-                <p class="mt-2 text-sm dark:text-gray-200">Click to change profile picture</p>
+                <p class="text-center mt-2 text-sm dark:text-gray-200">Click to change profile picture</p>
             </div>
         </div>
 
